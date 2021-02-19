e3c7b9fea1ee4f8f08b218f10e3961d6d422771c
Crypto-Advisor <adriennavarre@hotmail.com>
2021-02-19 14:58:55 -0800

    FarmToken progress

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.5.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    DappToken public dapptoken;
+    DaiToken public daitoken;
 
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+      dapptoken = _dappToken;
+      daitoken = _daiToken;
+    }
 }
